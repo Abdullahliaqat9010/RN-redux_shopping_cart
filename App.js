a911ab1c8a7939bb891634ec6f82d5f2842304a2
Abdullah <21100270@lums.edu.pk>
2020-09-01 03:24:53 +0500

    intial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>React Native</Text>
+    </View>
+  );
+};
+export default App;
